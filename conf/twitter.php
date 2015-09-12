dcfa8018f0c12dc044e51eebf352e02f2e5394e4
Peter Ayoub <peters.staying@gmail.com>
2015-09-12 18:57:35 +1000

    Added basic configuration parameters

--- conf/twitter.php
+++ conf/twitter.php
@@ -7,7 +7,21 @@
 */
 
 return array(
-        // TODO
+
+        /**  
+         *  OAuth settings which can be found in your twitter account
+         *  after creating an app. See https://apps.twitter.com
+         */      
+        'auth'  =>  array(
+                        'oauth_access_token'          =>  '',
+                        'oauth_access_token_secret'   =>  '',
+                        'consumer_key'                =>  '',
+                        'consumer_secret'             =>  ''
+                    ),
+        /**
+         *  Specifies the number of tweets to retrieve from a users timeline
+         */
+        'tweets_to_retrieve'  => 500
 );
 
 
