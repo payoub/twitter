86ce4485a81d0caecac3d379c6a5ed7733005a0a
Peter Ayoub <peters.staying@gmail.com>
2015-09-12 16:41:29 +1000

    Created framework for ajax requests to backend

--- app/main.php
+++ app/main.php
@@ -1,3 +1,5 @@
 <?php
 
 // TODO
+
+echo json_encode( array( 'status' => true, 'payload' => "<img src='http://placehold.it/350x350'/>") );
--- js/main.js
+++ js/main.js
@@ -1,1 +1,37 @@
 console.log('main.js loaded');
+
+//Set up event handlers
+jQuery(document).ready( function( $ ) {
+
+    //Form submit handler
+    $( "#form-submit" ).click( function() {
+
+        var usrinput = $( "#screen_name" ).val();
+
+        if ( usrinput.length > 0 ) {
+
+            var request = $.ajax({
+                method: "POST",
+                url: 'app/main.php',
+                data: { screen_name: usrinput },
+                dataType: "JSON"
+            });
+
+        } else {
+            console.log('TODO: Display error message that field is required');
+        }
+
+        request.done( function( msg ) {
+            console.log( msg );
+            if( msg.status == true ){
+                console.log('ajax completed successfully:'+msg.payload);
+                $( "#chart_div" ).html( msg.payload );
+            } else {
+                console.log('ajax completed with errors: '+msg.payload);
+            }
+
+        });      
+
+    }); //end submit handler
+
+}); //end document ready
