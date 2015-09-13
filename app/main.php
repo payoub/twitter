88e20afb34d65ff743ad04b7ea0b94480080136e
Peter Ayoub <peters.staying@gmail.com>
2015-09-13 14:16:07 +1000

    Refactored code to throw exceptions

--- app/main.php
+++ app/main.php
@@ -11,57 +11,51 @@ $auth = $config['auth'];
 
 /**
 * Checks if a screen name exists on the twitter platform
-* Exits if name does not exist 
 */
+
 function check_screen_name_exists( $name ) {
   //TODO
   $name_found = true;
   if( $name_found === false ) {
-    exit( json_encode( array( 'status' => false , 'payload' => 'Screen name does not exist') ) );
+    throw new Exception('Screen name does not exist');
   }
 }
 
 /**
 * Checks if an error has been returned from the twitter api
-* Returns error message or False
 */
 
 function request_error_exists( $data ) {
-  // TODO
+
   if( isset( $data->errors ) ){
-    return $data->errors[0]->message;
+    throw new Exception('Twitter responded - '.$data->errors[0]->message);
   }
-  return false;
+
 }
 
-# Sanitise input
-$sanitise_pattern = '/[^A-Za-z0-9_]/';
-$screen_name = preg_replace($sanitise_pattern,'',$_REQUEST['screen_name']);
-
-# Check if screen name exists
-check_screen_name_exists( $screen_name );
-
-# Create request uri to send to twitter api.
-$api_base_url = $config['base_url'] . "/statuses/user_timeline.json";
-$request_uri = "?screen_name=" . $screen_name . "&count=" . $config['tweets_to_retrieve'];
-
 try {
+
+  # Sanitise input
+  $sanitise_pattern = '/[^A-Za-z0-9_]/';
+  $screen_name = preg_replace($sanitise_pattern,'',$_REQUEST['screen_name']);
+  
+  # Check if screen name exists
+  check_screen_name_exists( $screen_name );
+  
+  # Create request uri to send to twitter api.
+  $api_url = $config['base_url'] . "/statuses/user_timeline.json";
+  $request_uri = "?screen_name=" . $screen_name . "&count=" . $config['tweets_to_retrieve'];
 
   # Launch request to twitter
   $twitter = new TwitterAPIExchange($auth);
   $response = $twitter->setGetfield( $request_uri )
-                      ->buildOauth( $api_base_url, 'GET' )
+                      ->buildOauth( $api_url, 'GET' )
                       ->performRequest(); 
 
   $data = json_decode( $response );
   
   # Check if reponse contains errors
-  $error = request_error_exists( $data );
-  if( $error !== false ) {
-
-    exit ( json_encode( array( 'status' => false, 'payload' => $error ) ) );
-
-  }
+  request_error_exists( $data );
 
   # Transform data into histogram input
   
@@ -82,6 +76,6 @@ try {
   
 } catch ( Exception $e ) {
 
-  exit ( json_encode( array( 'status' => false, 'payload' => 'Something went wrong while trying to contact twitter. '.$e->getMessage() ) ) );
+  exit ( json_encode( array( 'status' => false, 'payload' => 'Error: '.$e->getMessage() ) ) );
 
 }
